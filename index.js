94db87fbd72a5c34422e51a2bb5e2b8698e25062
ZeekzCantCode <71738306+ZeekzCantCode@users.noreply.github.com>
2020-11-11 22:42:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,47 @@
-const config = require("./config.json");
 const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
-const { MessageEmbed } = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 const fs = require("fs");
-const mongoose = require('mongoose')
-const prefix = config.prefix
-const db = require('quick.db')
-const Streamrole = require('discord-streamrole')
-const tic = require('discord-tictactoe')
+const config = require('./config.json')
+const prefix = config.prefix;
+const mongoose = require('mongoose');
+const chalk = require('chalk')
+const bot = new Discord.Client({disableMentions:'everyone'});
 
 bot.prefix = prefix;
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-mongoose.connect(config.Mongo,{
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-})
+bot.models = { user: require('./src/database/models/user.js') }
+require('./src/database/connect')
 
-const TempChannels = require("discord-temp-channels");
-const tempChannels = new TempChannels(bot);
- 
-// Register a new main channel
-tempChannels.registerChannel("769086230297182218", {
-    childCategory: "769087206483820555",
-    childAutoDeleteIfEmpty: true,
-    childAutoDeleteIfOwnerLeaves: true,
-    childBitrate: 64000,
-    childMaxUsers: 25,
-    childFormat: (member, count) => `🔒 ${member.user.username}'s Lounge`
-});
 
-bot.categories = fs.readdirSync("./src/commands/");
+bot.categories = fs.readdirSync("./src/commands");
 ["command"].forEach(handler => {
     require(`./src/handlers/${handler}`)(bot);
 });
 
-bot.on("ready", () => {
-  function randomStatus() {
-    let Playstatus = [`Vertix Studios`]
-    let Pstatus = Math.floor(Math.random() * Playstatus.length);
-    
-    bot.user.setActivity(Playstatus[Pstatus], {type: "WATCHING",});
-  }; setInterval(randomStatus, 12000) // Time in ms. 30000ms = 30 seconds. Min: 20 seconds, to avoid ratelimit.
 
-  console.log(`Vertix Studioss is now online on ${bot.guilds.cache.size} servers watching ${bot.users.cache.size} users`)
-
+bot.on("ready", (bot) => {
+     var da = new Date()
+    const Webhook = new Discord.WebhookClient(config.readyWebhookID, config.readyWebhookToken)
+    Webhook.send(`[\`${da.toLocaleDateString()}\`] [\`${da.toLocaleTimeString()}\`] **[ONLINE]** Vertix Studios reported online!`)
+  require('./src/events/client/ready')
 });
-
-Streamrole(bot, {
-    live: "🎥Streaming",
-    event: true,
-    console: true
-});
-
-new tic({
-  language: 'en',
-  command: '.ttt'
-}, bot);
 
 bot.on("message", async (message) => {
-  require("./src/events/guild/message")(bot, message);
+    require("./src/events/guild/message")(bot, message);
 });
 
-bot.on("guildMemberAdd", member => {
+bot.on("guildMemberAdd", async (member) => {
+    var d = new Date()
+    bot.channels.cache.get("776334966241624084").send(`[\`${d.toLocaleTimeString()}\`] 📥 ${member.user.tag} (\`${member.id}\`) joined the server, account created \`${member.user.createdAt.toLocaleString()}\`.`)
+    
+})
 
-  let welcomeEmbed = new MessageEmbed()
-  .setAuthor(`${member.user.tag} has just landed!`, member.user.displayAvatarURL())
-  .setDescription(`Welcome to our server ${bot.users.cache.get(member.id).toString(`${bot.users.cache.get(member.id).id}`)}. Enjoy your stay!`)
-  .setFooter(`Member: #${member.guild.memberCount} | Account Created ${member.user.createdAt.toLocaleString()}`, member.guild.iconURL())
-  .setColor('367fdd')
 
-  bot.channels.cache.get('692102883923066890').send(`<@&736071720427061298>`, welcomeEmbed)
+bot.on("guildMemberRemove", async (member) => {
+    var d = new Date()
+    bot.channels.cache.get("776334966241624084").send(`[\`${d.toLocaleTimeString()}\`] 📤 ${member.user.tag} (\`${member.id}\`) left the server, joined on \`${member.joinedAt.toLocaleDateString()}\`.`)
 })
 
 bot.login(config.Token)
